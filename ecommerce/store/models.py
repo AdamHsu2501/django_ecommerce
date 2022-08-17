6fa18dfd255a9eb0832be9629c8f9025d9971b4e
Adam Hsu <adamhsu2501@gmail.com>
2022-08-17 18:46:00 +0800

    Add cart functionality

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -3,24 +3,26 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
+
 class Customer(models.Model):
-    user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
+    user = models.OneToOneField(
+        User, null=True, blank=True, on_delete=models.CASCADE)
     name = models.CharField(max_length=200, null=True)
     email = models.CharField(max_length=200, null=True)
-    
+
     def __str__(self):
         return self.name
-    
-    
+
+
 class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.FloatField()
     digital = models.BooleanField(default=False, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
-    
+
     def __str__(self):
         return self.name
-    
+
     @property
     def imageURL(self):
         try:
@@ -28,33 +30,47 @@ class Product(models.Model):
         except:
             url = ''
         return url
-    
+
+
 class Order(models.Model):
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, null=True, blank=True)
+    customer = models.ForeignKey(
+        Customer, on_delete=models.CASCADE, null=True, blank=True)
     date_ordered = models.DateTimeField(auto_now_add=True)
     complete = models.BooleanField(default=False)
     transaction_id = models.CharField(max_length=100, null=False)
-    
+
     def __str__(self):
         return str(self.id)
-    
+
+    @property
+    def shipping(self):
+        shipping = False
+        order_items = self.orderitem_set.all()
+        for i in order_items:
+            if i.product.digital == False:
+                shipping = True
+                break
+        return shipping
+
     @property
     def get_cart_total(self):
         order_items = self.orderitem_set.all()
         total = sum([item.get_total for item in order_items])
         return total
-    
+
     @property
     def get_cart_items(self):
         order_items = self.orderitem_set.all()
         total = sum([item.quantity for item in order_items])
         return total
+
+
 class OrderItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
     quantity = models.IntegerField(default=0, null=False, blank=False)
     date_added = models.DateTimeField(auto_now_add=True)
-    
+
     @property
     def get_total(self):
         total = self.product.price * self.quantity
@@ -62,7 +78,8 @@ class OrderItem(models.Model):
 
 
 class ShippingAddress(models.Model):
-    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+    customer = models.ForeignKey(
+        Customer, on_delete=models.SET_NULL, null=True)
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
     address = models.CharField(max_length=200, null=False)
     city = models.CharField(max_length=200, null=False)
@@ -70,7 +87,6 @@ class ShippingAddress(models.Model):
     zipcode = models.CharField(max_length=200, null=False)
     country = models.CharField(max_length=200, null=False)
     date_added = models.DateTimeField(auto_now_add=True)
-    
+
     def __str__(self):
         return self.address
-    
--- ecommerce/store/urls.py
+++ ecommerce/store/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import store, cart, checkout
+from .views import store, cart, checkout, updateItem, processOrder
 
 urlpatterns = [
     path('', view=store, name='store'),
     path('cart/', view=cart, name='cart'),
-    path('checkout/', view=checkout, name='checkout')
+    path('checkout/', view=checkout, name='checkout'),
+
+    path('update_item/', view=updateItem, name='update_item'),
+    path('process_order/', view=processOrder, name='process_order'),
 ]
